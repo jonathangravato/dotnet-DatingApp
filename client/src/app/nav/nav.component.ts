ce11b74ad9d5e24e6374b7441dd29f1039a3495e
JonathanGravato <jgravato82@gmail.com>
2022-03-22 12:51:37 -0400

    updates

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -10,6 +10,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
+  user: any;
 
   constructor(public accountService: AccountService) { }
 
